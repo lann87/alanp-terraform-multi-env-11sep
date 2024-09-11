dfff068a269f96770e0fd91a32c32d130c47a423
Alan Peh <alanpeh87@gmail.com>
2024-09-11 21:37:33 +0800

    Initial commit

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@
 terraform {
   backend "s3" {
     bucket = "sctp-ce6-tfstate"
-    key    = "luqman-tf-workspace-act.tfstate"   #Change the value of this to yourname-tf-workspace-act.tfstate for  example
+    key    = "alanp-tf-workspace-act-11sep.tfstate"   #Change the value of this to yourname-tf-workspace-act.tfstate for  example
     region = "ap-southeast-1"
   }
 }
--- prod.tfvars
+++ prod.tfvars
@@ -1,4 +1,4 @@
 bucket_region = "sg"
 bucket_env = "prd"
-bucket_name = "luqman"
+bucket_name = "alanp"
 bucket_ran_num = "789"
